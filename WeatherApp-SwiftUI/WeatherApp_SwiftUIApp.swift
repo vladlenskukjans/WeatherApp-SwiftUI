842286609b85365bf2e047512ae1d4d60d3e256a
Vladlens Kukjans <kukjanvladlen@gmail.com>
2023-04-05 20:08:18 +0100

    Initial Commit

---
+++ WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Vladlens Kukjans on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
